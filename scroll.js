bfd88c94f0252c2fc19bcdbf55fbe94fd2ce8403
Bracer9 <87951030+Bracer9@users.noreply.github.com>
2022-05-30 20:26:16 +0900

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,4 @@
+$('#container').scrollgress({//バーの高さの基準となるエリア指定
+    height: '5px',//バーの高さ
+    color: 'rgb(10, 200, 200)',//バーの色
+});
---
+++ trigger.js
@@ -0,0 +1,22 @@
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function fadeAnime(){
+
+// くるっ
+
+  $('.rotateYTrigger').each(function(){ //rotateYTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('rotateY');// 画面内に入ったらrotateYというクラス名を追記
+    }else{
+    $(this).removeClass('rotateY');// 画面外に出たらrotateYというクラス名を外す
+    }
+    });
+
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
